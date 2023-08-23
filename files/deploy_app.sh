55e74f0bd8bb9ef616d7eeb28ce581510d6a1ae0
matthys24@hotmail.com <matthys24@hotmail.com>
2023-08-23 03:41:42 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,9 +11,10 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.notebookcheck.net%2FLatest-Cybertruck-videos-show-most-complete-version-yet-of-Tesla-s-steel-pickup-obvious-flaws-and-oddities-persist-while-production-date-looms.718251.0.html&psig=AOvVaw1lTvvmBrGIQtgxpaXJEiKN&ust=1692848460452000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCKD2roru8YADFQAAAAAdAAAAABAE"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app.
+  This page was generated with Terraform, Github and Coke Zero
   <!-- END -->
 
   </div>
